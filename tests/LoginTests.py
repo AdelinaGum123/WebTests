33c3d7098b675721fec57272c5889be93a3c00cd
AdelinaGum123 <gumerovarths@gmail.com>
2025-08-19 18:25:50 +0300

    Обернула созданные ранее тесты и функции в отчет алюр

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,3 +1,5 @@
+import allure
+
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
@@ -6,12 +8,17 @@ BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Введите логин'
 EMPTY_PASSWORD_ERROR = 'Введите пароль'
 
+
+@allure.suite('Проверка формы ввода логина и пароля')
+@allure.title('Проверка ошибки при пустых инпутах логина и пароля')
 def test_error_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+@allure.suite('Проверка формы ввода логина и пароля')
+@allure.title('Проверка ошибки при пустом инпуте пароля')
 def test_error_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
